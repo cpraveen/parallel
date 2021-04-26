eb81d2107be2c7d312e28859209f8bb2d014177e
Praveen C <cpraveen@gmail.com>
2021-04-26 14:56:01 +0530

    Print hostname

--- mpi/hello.c
+++ mpi/hello.c
@@ -2,13 +2,16 @@
 #include <mpi.h>
 
 int main(int argc, char** argv) {
-   int rank, size, ierr;
+   int rank, size, ierr, plen;
+   char pname[MPI_MAX_PROCESSOR_NAME];
 
    ierr = MPI_Init(&argc, &argv);
    ierr = MPI_Comm_size(MPI_COMM_WORLD, &size);
    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   ierr = MPI_Get_processor_name(pname, &plen);
 
-   printf("Hello World, I am rank %d of %d procs\n", rank, size);
+   printf("Hello World, from rank %d of %d procs, host %s\n", rank, size,
+          pname);
    ierr = MPI_Finalize();
    return 0;
 }
--- mpi/hello.f90
+++ mpi/hello.f90
@@ -1,11 +1,15 @@
 program mpitest
    use MPI
-   integer :: rank, size, ierr
+   integer :: rank, size, ierr, plen
+   character*(MPI_MAX_PROCESSOR_NAME) :: pname
 
    call MPI_Init(ierr)
    call MPI_Comm_size(MPI_COMM_WORLD, size, ierr)
    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+   call MPI_Get_processor_name(pname, plen, ierr)
 
-   write(*,*) 'Hello World, I am rank ',rank,' of ',size,' procs'
+   write(*,'("Hello World, from rank", I5, " of ", I5, " procs")', &
+         ADVANCE="NO") rank, size
+   write(*,'(", host ", A)') trim(pname)
    call MPI_Finalize(ierr)
 end
